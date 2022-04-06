a675f4e873b1d39371f0cdd0b749eba7b883bb4e
kevinashley <kashley3141@gmail.com>
2022-04-05 18:52:53 -0700

    configured dev linter and formatting enforcement

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
+/** @format */
+
 import React from "react";
 import {
-  createTheme,
-  styled,
-  ThemeProvider,
-  darken,
+    createTheme,
+    styled,
+    ThemeProvider,
+    darken,
 } from "@mui/material/styles";
+
 import GlobalStyles from "@mui/material/GlobalStyles";
 import Navbar from "./components/navbar";
 
 const customTheme = createTheme({
-  palette: {
-    primary: {
-      main: "#5a4dff",
+    palette: {
+        primary: {
+            main: "#5a4dff",
+        },
     },
-  },
 });
 
 const globalCss = {
-  body: {
-    margin: 0,
-  },
+    body: {
+        margin: 0,
+    },
 };
 
 function App() {
-  return (
-    <ThemeProvider theme={customTheme}>
-      <GlobalStyles styles={globalCss} />
-      <Navbar />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={customTheme}>
+            <GlobalStyles styles={globalCss} />
+            <Navbar />
+        </ThemeProvider>
+    );
 }
 
 export default App;
