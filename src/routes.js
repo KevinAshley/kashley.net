9da6b64f5adf52f7ff8e7cdaeb059092aa4ef39b
kevinashley <kashley3141@gmail.com>
2022-04-16 22:55:12 -0700

    more vsconfig

--- src/routes.js
+++ src/routes.js
@@ -4,39 +4,36 @@ import { loadableOptions, handleImportError } from "./utils";
 import loadable from "@loadable/component";
 import HomeIcon from "@mui/icons-material/Home";
 
-const Home = loadable(
-    () => import("./pages/home").catch(handleImportError),
-    loadableOptions
-);
+const Home = loadable(() => import("./pages/home"), loadableOptions);
 
 export const mainRoutes = [
-    {
-        label: "Home",
-        component: Home,
-        route: "/",
-        icon: HomeIcon,
-    },
-    {
-        label: "Contact Me",
-        component: Home,
-        route: "/contact-me/",
-        icon: HomeIcon,
-    },
+  {
+    label: "Home",
+    component: Home,
+    route: "/",
+    icon: HomeIcon,
+  },
+  {
+    label: "Contact Me",
+    component: Home,
+    route: "/contact-me/",
+    icon: HomeIcon,
+  },
 ];
 
 export const portfolioRoutes = [
-    {
-        label: "Test",
-        component: Home,
-        route: "/test/",
-        icon: HomeIcon,
-    },
-    {
-        label: "Cricket Scoreboard",
-        component: Home,
-        route: "/cricket-scoreboard/",
-        icon: HomeIcon,
-    },
+  {
+    label: "Test",
+    component: Home,
+    route: "/test/",
+    icon: HomeIcon,
+  },
+  {
+    label: "Cricket Scoreboard",
+    component: Home,
+    route: "/cricket-scoreboard/",
+    icon: HomeIcon,
+  },
 ];
 
 export const routes = mainRoutes.concat(portfolioRoutes);
