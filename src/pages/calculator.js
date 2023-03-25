58d2903f9c6465684aa4cc654c5f41b4c2ab788c
kevinashley <kashley3141@gmail.com>
2023-03-25 15:03:18 -0700

    calculator progress

--- src/pages/calculator.js
+++ src/pages/calculator.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet";
 import { companyName } from "../vars";
 import Paper from "@mui/material/Paper";
@@ -29,6 +29,48 @@ const numKeysArray = new Array(9).fill(0);
 const Home = (props) => {
     const { label } = props;
     const classes = useStyles();
+    const [total, setTotal] = useState(0);
+    const [operator, setOperator] = useState();
+    const [operand, setOperand] = useState();
+
+    const handleNumberPress = (num) => {
+        let newNumber = operator
+            ? Number((operand || 0).toString() + num.toString())
+            : Number(total.toString() + num.toString());
+        if (operator) {
+            setOperand(newNumber);
+        } else {
+            setTotal(newNumber);
+        }
+    };
+
+    const handleOperatorPress = (op) => {
+        setOperator(op);
+    };
+
+    const handleClear = () => {
+        setTotal(0);
+        setOperand(null);
+        setOperator(null);
+    };
+
+    const equals = () => {
+        let newTotal = 0;
+        if (operator === "+") {
+            newTotal = Number(total) + Number(operand);
+        } else if (operator === "-") {
+            newTotal = Number(total) - Number(operand);
+        } else if (operator === "*") {
+            newTotal = Number(total) * Number(operand);
+        } else if (operator === "/") {
+            newTotal = Number(total) / Number(operand);
+        }
+        setTotal(newTotal);
+        setOperator(null);
+        setOperand(null);
+    };
+    console.log(total, operator, operand);
+
     return (
         <div>
             <Helmet>
@@ -43,19 +85,28 @@ const Home = (props) => {
                     type="number"
                     fullWidth
                     disabled
-                    value={0}
+                    value={operand ? operand : total}
                     className={classes.displayWindow}
                 />
                 <Grid container spacing={1}>
                     <Grid item xs={9}>
                         <Grid container spacing={1} className={classes.numbers}>
                             <Grid item xs={4}>
-                                <Button variant="contained" color="error" fullWidth>
+                                <Button
+                                    variant="contained"
+                                    color="error"
+                                    fullWidth
+                                    onClick={handleClear}
+                                >
                                     C
                                 </Button>
                             </Grid>
                             <Grid item xs={4}>
-                                <Button variant="contained" fullWidth>
+                                <Button
+                                    variant="contained"
+                                    fullWidth
+                                    onClick={() => handleNumberPress(0)}
+                                >
                                     0
                                 </Button>
                             </Grid>
@@ -67,9 +118,10 @@ const Home = (props) => {
 
                             {numKeysArray.map((x, index) => {
                                 const numValue = index + 1;
+                                const handleClick = () => handleNumberPress(numValue);
                                 return (
                                     <Grid item xs={4} key={index}>
-                                        <Button variant="contained" fullWidth>
+                                        <Button variant="contained" fullWidth onClick={handleClick}>
                                             {numValue}
                                         </Button>
                                     </Grid>
@@ -79,25 +131,17 @@ const Home = (props) => {
                     </Grid>
                     <Grid item xs={3}>
                         <Grid container spacing={1}>
-                            {[
-                                {
-                                    label: "*",
-                                },
-                                {
-                                    label: "/",
-                                },
-                                {
-                                    label: "+",
-                                },
-                                {
-                                    label: "-",
-                                },
-                            ].map((a, index) => {
-                                const { label } = a;
+                            {["*", "/", "+", "-"].map((a, index) => {
+                                const handleClick = () => handleOperatorPress(a);
                                 return (
                                     <Grid item xs={12} key={index}>
-                                        <Button variant="contained" color="secondary" fullWidth>
-                                            {label}
+                                        <Button
+                                            variant="contained"
+                                            color="secondary"
+                                            fullWidth
+                                            onClick={handleClick}
+                                        >
+                                            {a}
                                         </Button>
                                     </Grid>
                                 );
@@ -105,7 +149,7 @@ const Home = (props) => {
                         </Grid>
                     </Grid>
                     <Grid item xs={12}>
-                        <Button variant="contained" color="secondary" fullWidth>
+                        <Button variant="contained" color="secondary" fullWidth onClick={equals}>
                             =
                         </Button>
                     </Grid>
