05b6328dc899af77618b8244b51d42c344ed8c50
kevinashley <kashley3141@gmail.com>
2023-03-25 15:06:21 -0700

    removed pages under construction

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { loadableOptions, handleImportError } from "./utils";
 import loadable from "@loadable/component";
 import HomeIcon from "@mui/icons-material/Home";
 import MailIcon from "@mui/icons-material/Mail";
+import CalculateIcon from "@mui/icons-material/Calculate";
 
 const Home = loadable(() => import("./pages/home"), loadableOptions);
 const CricketScoreboard = loadable(() => import("./pages/cricketScoreboard"), loadableOptions);
@@ -30,20 +31,20 @@ export const portfolioRoutes = [
         label: "Calculator",
         component: Calculator,
         route: "/calculator/",
-        icon: HomeIcon,
+        icon: CalculateIcon,
     },
-    {
-        label: "Cricket Scoreboard",
-        component: CricketScoreboard,
-        route: "/cricket-scoreboard/",
-        icon: HomeIcon,
-    },
-    {
-        label: "Stocks",
-        component: Stocks,
-        route: "/stocks/",
-        icon: HomeIcon,
-    },
+    // {
+    //     label: "Cricket Scoreboard",
+    //     component: CricketScoreboard,
+    //     route: "/cricket-scoreboard/",
+    //     icon: HomeIcon,
+    // },
+    // {
+    //     label: "Stocks",
+    //     component: Stocks,
+    //     route: "/stocks/",
+    //     icon: HomeIcon,
+    // },
 ];
 
 export const routes = mainRoutes.concat(portfolioRoutes);
