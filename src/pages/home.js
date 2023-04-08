7adb702956f19d89b157bc6da93ecd1e1b6a0a18
kevinashley <kashley3141@gmail.com>
2023-04-08 15:45:05 -0700

    Homepage revisions

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,26 +5,29 @@ import { Helmet } from "react-helmet";
 import { companyName } from "../vars";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 // import Content from "../components/content";
+const backgroundGradient = "rgba(0,0,0,0.7)";
 
 const Home = () => {
     return (
         <Box
             sx={{
-                color: "red",
-                backgroundImage: 'url("/images/pexels-photo-12027143.jpeg")',
+                color: "#fff",
+                backgroundImage: `linear-gradient(0deg, ${backgroundGradient}, ${backgroundGradient}), url(/images/pexels-photo-12027143-cropped.jpg)`,
                 width: "100%",
-                height: "100%",
+                minHeight: "30vh",
                 backgroundSize: "cover",
+                backgroundPosition: "bottom",
                 padding: "2rem",
+                textShadow: "0px 0px 5px black",
             }}
         >
             <Helmet>
                 <title>Home | {companyName}</title>
                 <meta name="description" content="Home Page" />
             </Helmet>
-
-            <Paper
+            <Box
                 sx={{
                     maxWidth: 936,
                     margin: "auto",
@@ -34,9 +37,23 @@ const Home = () => {
                 }}
             >
                 <h1>Welcome!</h1>
-                <p>This website was built with React.js and the Material UI components library.</p>
-                <p>- Kevin Ashley.</p>
-            </Paper>
+                <p>
+                    Thanks for visiting my website. Check out my portfolio for examples of my work.
+                    All of the code for this website is available to review at github.
+                </p>
+                <Button
+                    variant="contained"
+                    href="https://github.com/KevinAshley/kashley.net"
+                    target="_blank"
+                >
+                    Github Repo
+                </Button>
+                <p>
+                    Kevin Ashley
+                    <br />
+                    Sacramento, CA
+                </p>
+            </Box>
         </Box>
     );
 };
